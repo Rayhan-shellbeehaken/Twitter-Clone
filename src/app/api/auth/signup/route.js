45e340ac6ed633664725eb0c3b64cf83c674eff6
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-01-22 10:00:05 +0600

    user model modification and signup api complete

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,14 +1,15 @@
 import { connect } from "@/app/db/db.config";
 import User from "@/app/models/user.model";
 import { NextResponse } from "next/server";
-import bcrypt, { genSalt, hash } from "bcryptjs";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 connect();
 
 export async function POST(request) {
     try{
         const reqBody = await request.json();
-        const { username, email, password } = reqBody;
+        const { username, email, password, dateofBirth } = reqBody;
 
         const user = await User.findOne({email : email});
 
@@ -16,21 +17,32 @@ export async function POST(request) {
             return NextResponse.json({message : 'User already exist.'},{status : 401});
         }
 
-        const salt = await genSalt(10);
+        const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
             username,
             email,
-            password : hashedPassword
+            password : hashedPassword,
+            dateofBirth
         });
 
         const savedUser = await newUser.save();
 
-        return NextResponse.json({
-            message : 'Registration successfully!',
-            savedUser
-        })
+        const payload = {
+            _id : savedUser._id,
+            username : savedUser.username,
+            email : savedUser.email
+        }
+
+        const token = jwt.sign(payload, process.env.SECRET_TOKEN, {expiresIn : '1h'});
+        const response = NextResponse.json({message : 'Registration successfully', savedUser});
+
+        response.cookies.set("next-auth.session-token",token,{
+            httpOnly : true
+        });
+
+        return response;
     }catch(error){
         console.log("Sign Up error occured :(");
         console.log(error);
--- src/app/models/user.model.js
+++ src/app/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : [true, "Please provide password"],
     },
+    dateofBirth : {
+        type : Date,
+        required : [true, "please provide date of birth"],
+    },
     profileImage : {
         type : String,
         validate: {
