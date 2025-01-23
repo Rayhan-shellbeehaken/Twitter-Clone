945cc0fe5d5c733decc04eb2a6dd537149f0b579
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-01-23 16:21:22 +0600

    footer modified

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,7 +4,7 @@ import styles from './css/footer.module.css';
 export default function Footer() {
     const items = ['About', 'Download the X app', 'Help Center', 'Terms of Service', 'Privacy Policy', 
         'Cookie Policy', 'Accessibility', 'Ads info', 'Blog', 'Careers', 'Brand Resources', 'Advertising',
-        'Marketing', 'X for Business', 'Developers', 'Directory', 'Settings', '&copy; 2025 X Corp'
+        'Marketing', 'X for Business', 'Developers', 'Directory', 'Settings', '© 2025 X Corp'
     ]
     return (
         <div className={styles["footer-container"]}>
