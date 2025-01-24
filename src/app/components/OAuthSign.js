2ae624b57ff90a6d5ae6f511e116d046f1fcb907
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-01-24 15:36:47 +0600

    Integrated Oauth with frontend

--- src/app/components/OAuthSign.js
+++ src/app/components/OAuthSign.js
@@ -3,17 +3,18 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
 import styles from './css/oauthsign.module.css';
 import buttonStyle from './css/button.module.css';
+import { doLogin } from '../helpers/authentication';
 
 export default function OAuthSign() {
   return (
     <>
-        <form className={styles["oauth-form"]}>
-            <button className={buttonStyle.button}>
+        <form action={doLogin} className={styles["oauth-form"]}>
+            <button className={buttonStyle.button} name='action' value='google'>
               <FcGoogle />
               <p>Sign up with Google</p> 
             </button>
 
-            <button className={buttonStyle.button}>
+            <button className={buttonStyle.button} name='action' value='github'>
               <FaGithub />
               <p>Sign up with github</p>
             </button>
