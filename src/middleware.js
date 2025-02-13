12d2f5ba6182f1ca64a5629f1f82cf3044f284ef
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-02-13 11:35:05 +0600

    post status router protected

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,8 @@ import { getToken } from 'next-auth/jwt';
 
 // This function can be marked `async` if using `await` inside
 const publicPaths = ["/"];
-const privatePaths = ["/home","/profile","/dateofbirth"]
+const privatePaths = ["/home","/profile","/dateofbirth"];
+const privateRegexPaths = [/^\/[^\/]+\/status\/[^\/]+$/]; 
 
 export async function middleware(request) {
     try{
@@ -11,6 +12,7 @@ export async function middleware(request) {
         console.log(currentPath);
         const token = await getToken({ req: request, secret: process.env.SECRET_TOKEN });
         console.log(token);
+        const isPrivate = privatePaths.includes(currentPath) || privateRegexPaths.some((regex) => regex.test(currentPath));
         if(token && publicPaths.includes(currentPath)){
             if(token.isNewUser){
                 return NextResponse.redirect(new URL("/dateofbirth", request.url));
@@ -18,7 +20,7 @@ export async function middleware(request) {
                 return NextResponse.redirect(new URL(`/home?feed=foryou`, request.url));
             }
         }
-        else if(!token && privatePaths.includes(currentPath)){
+        else if(!token && isPrivate){
             return NextResponse.redirect(new URL("/", request.url));
         }
     }catch(error){
@@ -28,5 +30,5 @@ export async function middleware(request) {
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/","/home","/dateofbirth","/profile"],
+  matcher: ["/","/home","/dateofbirth","/profile","/:name/status/:tweetId*"],
 }
