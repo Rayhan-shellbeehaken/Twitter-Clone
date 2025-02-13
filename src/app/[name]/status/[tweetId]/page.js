9cd5b2c1772e0827d0b1e3532d3200767e4c882f
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-02-13 11:25:40 +0600

    Back option created for post status

--- src/app/[name]/status/[tweetId]/page.js
+++ src/app/[name]/status/[tweetId]/page.js
@@ -5,13 +5,20 @@ import SearchBox from '@/app/components/searchbox/SearchBox'
 import Trending from '@/app/components/trending/Trending'
 import TermsAndCondition from '@/app/components/termsandcondition/TermsAndCondition'
 import ReleventPeople from '@/app/components/releventpeople/ReleventPeople'
+import { GoArrowLeft } from "react-icons/go";
+import Link from 'next/link'
 
 export default function page() {
     return (
         <ProtectedLayout>
             <div className={styles.page}>
                 <div className={styles.left}>
-
+                    <div className={styles.head}>
+                        <Link href="/home" className={styles.back}>
+                            <GoArrowLeft/>
+                        </Link>
+                        <div>Post</div>
+                    </div>
                 </div>
                 <div className={styles.right}>
                     <SearchBox/>
