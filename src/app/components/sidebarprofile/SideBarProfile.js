b42eaefa45d1a9f894b8dcdb8771209efd4fc502
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-02-26 16:36:52 +0600

    user info added to sidebar

--- src/app/components/sidebarprofile/SideBarProfile.js
+++ src/app/components/sidebarprofile/SideBarProfile.js
@@ -1,25 +1,44 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './sidebarprofile.module.css'
 import xlogo from '../../../../public/images/xprofile.png';
 import Image from 'next/image';
 import { RiMoreFill } from "react-icons/ri";
 import SideBarPopUp from '../sidebarpopup/SideBarPopUp';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function SideBarProfile() {
     const [showPopUp, setShowPopUp] = useState(false);
+    const [userInfo, setUserInfo] = useState({name : '' , profileImage : ''});
+
+    useEffect(()=>{
+        async function fetchData() {
+            try{
+                const user = await axios.get('/api/user');
+                console.log(user);
+                setUserInfo({name : user.data.user.username , profileImage : user.data.user.profileImage});
+            }catch(error){
+                console.log(error);
+            }
+        }
+        fetchData();
+    },[])
+
     return (
         <>
             {showPopUp && <SideBarPopUp/>} 
             <div className={styles.profile} onClick={() => setShowPopUp(!showPopUp)}>
                 <div className={styles["profile-left"]}>
                     <div className={styles["profile-image"]}>
-                        <Image src={xlogo} alt='xlogo' priority layout='intrinsic'></Image>
+                        {userInfo.profileImage !== '' && 
+                            <img src={userInfo.profileImage} alt='xlogo'></img>
+                        }
+                        
                     </div>
                     <div className={styles["profile-info"]}>
-                        <h3>Rayhan</h3>
-                        <p>@_Rayhan66</p>
+                        <h3>{userInfo.name}</h3>
+                        <p>@_{userInfo.name}</p>
                     </div>
                 </div>
                 
